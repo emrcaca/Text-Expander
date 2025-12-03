67d42b599399a03aadda20d0d639b5df39710b9f
emrcaca <emrcaca@yahoo.com>
2025-12-03 19:44:17 +0300

    Update and rename userscript.js to Userscript.js

--- Userscript.js
+++ Userscript.js
@@ -11,9 +11,9 @@
     "use strict";
 
     const EXPANSIONS = {
-        '"1': "Owo h",
-        '"2': "Owo b",
-        '"3': "Owo",
+        'slm': "Selam!",
+        'nbr': "Naber?",
+        ':mail': "example.mail.com",
         ':cp': 'Kopyalanan: {{clipb}}',
         ':time': '{{time}}',
         ':date': '{{date}}',
